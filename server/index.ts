c8c663d92428f7c052b458e5394142120e60cc16
testdiageo <92740123+testdiageo@users.noreply.github.com>
2025-10-13 15:33:17 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
-﻿import express from "express";
+﻿import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
